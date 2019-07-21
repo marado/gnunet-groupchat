a47e5da8eab69f54a4e679ec3c3e8a77973503d2
lurchi <lurchi@strangeplace.net>
2019-07-21 10:43:37 +0200

    remember focusIndex after unfocus by using absolute indices

--- src/listtile.nim
+++ src/listtile.nim
@@ -14,8 +14,8 @@ type ListElement* = object
 
 type ListTile* = ref object of Tile
   elements: seq[ListElement]
-  displayBottomIndex: int
   focusIndex: int
+  focusBottom: int
   elementSpacing*: int
 
 proc drawBox*(topLeft: Point, bottomRight: Point) =
@@ -32,6 +32,10 @@ proc drawBox*(topLeft: Point, bottomRight: Point) =
   setCursorPos(bottomRight.x, bottomRight.y)
   stdout.write("┘")
 
+proc bottom(tile: ListTile): int =
+  if tile.focussed: tile.focusBottom
+  else: max(0, tile.elements.len() - 1)
+
 proc elementHeight(tile: ListTile, element: ListElement): int =
   result = 1 + tile.elementSpacing
   if element.content.len() > 0:
@@ -43,50 +47,48 @@ proc elementCount*(tile: ListTile): int =
   if tile.elements.len() == 0:
     return
   var consumedHeight = 0
-  var nextHeight = tile.elementHeight(tile.elements[tile.displayBottomIndex])
+  var nextHeight = tile.elementHeight(tile.elements[tile.bottom()])
   proc hasSpace(): bool =
     consumedHeight + nextHeight - tile.elementSpacing <= tile.height() - 2
   while result < tile.elements.len() and hasSpace():
     consumedHeight = consumedHeight + nextHeight
     result.inc()
-    let nextIndex = tile.displayBottomIndex - result
+    let nextIndex = tile.bottom() - result
     if nextIndex >= 0:
       nextHeight = tile.elementHeight(tile.elements[nextIndex])
 
-proc element*(tile: ListTile, index: int): ListElement =
-  let realIndex = tile.displayBottomIndex - tile.elementCount() + 1 + index
-  tile.elements[realIndex]
-
 proc elementPos(tile: ListTile, index: int): Point =
-  assert(index >= 0 and index <= tile.elementCount())
+  assert(index >= 0 and index <= tile.elements.len())
   let firstElemY = tile.topLeft().y + 1
-  proc height(i: int): int = tile.elementHeight(tile.element(i))
+  proc height(i: int): int = tile.elementHeight(tile.elements[i])
+  let topIndex = tile.bottom() - tile.elementCount() + 1
   result.x = tile.topLeft().x + 1
-  result.y = toSeq(0 .. (index - 1)).map(height).foldl(a + b, firstElemY)
+  result.y = toSeq(topIndex .. (index - 1)).map(height).foldl(a + b, firstElemY)
 
 proc focusElement(tile: ListTile, index: int) =
-  assert(index >= 0 and index < tile.elementCount())
+  assert(index >= 0 and index <= tile.bottom())
   let pos = tile.elementPos(index)
   writeLeftAligned(pos,
                    tile.width() - 2,
-                   tile.element(index).title,
+                   tile.elements[index].title,
                    1,
                    fgDefault,
                    bgRed)
  
 proc unfocusElement(tile: ListTile, index: int) =
-  assert(index >= 0 and index < tile.elementCount())
+  assert(index >= 0 and index <= tile.bottom())
   let pos = tile.elementPos(index)
   writeLeftAligned(pos,
                    tile.width() - 2,
-                   tile.element(index).title,
+                   tile.elements[index].title,
                    1,
                    fgCyan,
                    bgDefault)
 
 proc presentElement(tile: ListTile, index: int) =
+  assert(index >= 0 and index < tile.elements.len())
   tile.unfocusElement(index)
-  let element = tile.element(index)
+  let element = tile.elements[index]
   if element.content.len() > 0:
     let lines = element.content.wrapWords(tile.width() - 4).splitLines()
     let pos = tile.elementPos(index)
@@ -95,73 +97,81 @@ proc presentElement(tile: ListTile, index: int) =
 
 method focus*(this: ListTile) =
   this.focussed = true
+  this.clear()
   stdout.setForegroundColor(fgRed)
   drawBox(this.topLeft(), this.bottomRight())
   resetAttributes()
-  if this.elementCount() > 0:
+  if this.elements.len() > 0:
+    let topIndex = this.bottom() - this.elementCount() + 1
+    for i in topIndex .. this.bottom():
+      this.presentElement(i)
     this.focusElement(this.focusIndex)
 
 method unfocus*(this: ListTile) =
   this.focussed = false
+  this.clear()
   drawBox(this.topLeft(), this.bottomRight())
-  if this.elementCount() > 0:
-    this.unfocusElement(this.focusIndex)
+  if this.elements.len() > 0:
+    let topIndex = this.bottom() - this.elementCount() + 1
+    for i in topIndex .. this.bottom():
+      this.presentElement(i)
 
 method present*(tile: ListTile) =
-  tile.clear()
-  for i in 0 .. (tile.elementCount() - 1):
-    tile.presentElement(i)
   if tile.focussed:
     tile.focus()
   else:
     tile.unfocus()
 
 proc focusNextElement*(tile: ListTile) =
-  let elementCount = tile.elementCount()
-  if elementCount == 0:
+  assert(tile.focussed)
+  if tile.elements.len() == 0:
     return
   tile.unfocusElement(tile.focusIndex)
-  if tile.focusIndex < elementCount - 1:
+  if tile.focusIndex < tile.focusBottom:
     tile.focusIndex.inc()
-  elif tile.displayBottomIndex < tile.elements.len() - 1:
-    tile.displayBottomIndex.inc()
-    tile.focusIndex = tile.elementCount() - 1
+  elif tile.focusIndex < tile.elements.len() - 1:
+    tile.focusIndex.inc()
+    tile.focusBottom.inc()
     tile.present()
   tile.focusElement(tile.focusIndex)
 
 proc focusPrevElement*(tile: ListTile) =
-  let elementCount = tile.elementCount()
-  if elementCount == 0:
+  assert(tile.focussed)
+  if tile.elements.len() == 0:
     return
   tile.unfocusElement(tile.focusIndex)
-  if tile.focusIndex > 0:
+  let elementCount = tile.elementCount()
+  if tile.focusIndex > tile.focusBottom - elementCount + 1:
     tile.focusIndex.dec()
-  elif tile.displayBottomIndex >= elementCount:
-    tile.displayBottomIndex.dec()
-    tile.focusIndex = 0
+  elif tile.focusBottom >= elementCount:
+    tile.focusBottom.dec()
+    tile.focusIndex = tile.focusBottom - elementCount + 1
     tile.present()
   tile.focusElement(tile.focusIndex)
 
-method processInput*(this: ListTile, ch: char) =
-  case ch
-  of 'j': this.focusNextElement()
-  of 'k': this.focusPrevElement()
-  else: discard
-
 proc addElement*(tile: ListTile, id: string, title: string, content = "") =
   tile.elements.add(ListElement(id: id, title: title, content: content))
-  tile.displayBottomIndex = tile.elements.len() - 1
+  if tile.focussed:
+    tile.focusBottom = max(tile.elementCount() - 1, tile.focusBottom)
+  else:
+    tile.focusBottom = tile.elements.len() - 1
+    tile.focusIndex = tile.focusBottom - tile.elementCount() + 1
   tile.present()
 
 proc deleteElement*(tile: ListTile, id: string) =
   for i in 0 .. tile.elements.len() - 1:
     if tile.elements[i].id == id:
       tile.elements.delete(i)
-      tile.displayBottomIndex = min(tile.elements.len() - 1, tile.displayBottomIndex)
-      tile.displayBottomIndex = max(0, tile.displayBottomIndex)
-      tile.focusIndex = min(tile.elementCount() - 1, tile.focusIndex)
+      tile.focusBottom = min(tile.elements.len() - 1, tile.focusBottom)
+      tile.focusBottom = max(0, tile.focusBottom)
+      tile.focusIndex = min(tile.elements.len() - 1, tile.focusIndex)
       tile.focusIndex = max(0, tile.focusIndex)
       tile.present()
       break
 
-
+method processInput*(this: ListTile, ch: char) =
+  case ch
+  of 'j': this.focusNextElement()
+  of 'k': this.focusPrevElement()
+  of 'd': this.deleteElement("x")
+  else: discard
